b2c2ddf73c6b26360e30528116bc20574cfdd4af
Frederick Pringle <freddyjepringle@gmail.com>
2025-01-26 21:48:18 +0100

    Make sure we register deallocation functions properly

--- hprocps/src/System/Proc.hs
+++ hprocps/src/System/Proc.hs
@@ -29,7 +29,6 @@ where
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Resource
-import Data.Foldable
 import qualified System.Proc.Bindings as B
 import System.Proc.Bindings.Error
 import System.Proc.Bindings.Info
@@ -63,32 +62,61 @@ rightToMaybe = either (const Nothing) Just
 
 -- DRY
 readNextProcEither :: ProcTab s -> RegionM s (Either ProcError (Proc s))
-readNextProcEither (UnsafeProcTab pt) =
-  liftIO (B.readNextProc pt) >>= \case
-    Left err -> pure $ Left err
-    Right proc -> do
-      _key <- register $ B.closeProc proc
-      pure $ Right $ UnsafeProc proc
+readNextProcEither (UnsafeProcTab _ pt) =
+  resourceMask_ $
+    liftIO (B.readNextProc pt) >>= \case
+      Left err -> pure $ Left err
+      Right proc -> do
+        key <- register $ B.closeProc proc
+        pure $ Right $ UnsafeProc proc key
 
 -- | Open a 'Proc' representing the current proces or task.
 readSelfProc :: RegionM s (Proc s)
 readSelfProc = UnsafeProc <$> allocateMEither B.openSelfProc B.closeProc
 
+resourceMask_ :: MonadResource m => ResourceT IO b -> m b
+resourceMask_ rio = resourceMask $ const rio
+
 {- | Read all the 'Proc's according to a 'TableConfig'.
 Errors are ignored.
 -}
 readAllProcsLenient :: TableConfig -> RegionM s [Proc s]
-readAllProcsLenient cfg = fmap UnsafeProc <$> allocateM (B.openAllProcsLenient cfg) (traverse_ B.closeProc)
+readAllProcsLenient cfg = resourceMask_ $ do
+  procs <- liftIO $ B.openAllProcsLenient cfg
+
+  forM procs $ \proc -> do
+    _key <- register (B.closeProc proc)
+    pure $ UnsafeProc proc
 
 -- | Read all the 'Proc's according to a 'TableConfig'.
 readAllProcs :: TableConfig -> RegionM s [Either ProcError (Proc s)]
-readAllProcs cfg = fmap (fmap UnsafeProc) <$> allocateM (B.openAllProcs cfg) (traverse_ (traverse_ B.closeProc))
+readAllProcs cfg = resourceMask_ $ do
+  procs <- liftIO $ B.openAllProcs cfg
+
+  forM procs $ \case
+    Left err -> pure $ Left err
+    Right proc -> do
+      _key <- register (B.closeProc proc)
+      pure $ Right $ UnsafeProc proc
 
 {- | Read all the 'Proc's according to a 'TableConfig'.
 Any errors are thrown using 'throwProcErrorT'.
 -}
 readAllProcsThrow :: TableConfig -> RegionM s [Proc s]
-readAllProcsThrow cfg = readAllProcs cfg >>= either throwProcErrorT pure . sequence
+readAllProcsThrow cfg = do
+  eProcs <- resourceMask_ $ do
+    eProcs <- liftIO $ B.openAllProcs cfg
+
+    case sequence eProcs of
+      Left err -> pure $ Left err
+      Right procs ->
+        fmap Right . forM procs $ \proc -> do
+          _key <- register (B.closeProc proc)
+          pure $ UnsafeProc proc
+
+  case eProcs of
+    Left err -> throwProcErrorT err
+    Right procs -> pure procs
 
 -- | Read the next 'ProcInfo' in the 'ProcTab'.
 readNextProcInfo :: MonadIO m => ProcTab s -> m (Either ProcError B.ProcInfo)
